e1e96533c0f3c99d2e14e6dc5c45afeb36476695
Kristen Lavavej <35638702+klavavej@users.noreply.github.com>
2021-08-18 16:32:15 -0700

    Create context.go

---
+++ netlify/functions/context/context.go
@@ -0,0 +1,16 @@
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	lc, ok := lambdacontext.FromContext(ctx)
+	if !ok {
+		return &events.APIGatewayProxyResponse{
+			StatusCode: 503,
+			Body:       "Something went wrong :(",
+		}, nil
+	}
+
+	cc := lc.ClientContext
+
+	return &events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "Hello, " + cc.Client.AppTitle,
+	}, nil
+}
