175e7839c481c56a2381dde31183fa4b6dcc2a05
Kristen Lavavej <35638702+klavavej@users.noreply.github.com>
2021-08-20 09:54:12 -0700

    Update context.go

--- netlify/functions/context/context.go
+++ netlify/functions/context/context.go
@@ -1,9 +1,11 @@
-package lambdacontext
+package main
 
 import (
+	"context"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"context"
+	"github.com/aws/aws-lambda-go/lambdacontext"
 )
 
 func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
@@ -21,4 +23,8 @@ func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*event
 		StatusCode: 200,
 		Body:       "Hello, " + cc.Client.AppTitle,
 	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
 }
