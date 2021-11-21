889d9c6d5611cd0ffafc0d3899bb4b2d77aac57d
Kristen Lavavej <35638702+klavavej@users.noreply.github.com>
2021-11-21 11:26:05 -0800

    Update netlify/functions/builder/builder.js

--- netlify/functions/builder/builder.js
+++ netlify/functions/builder/builder.js
@@ -13,14 +13,6 @@ async function handler(event, context) {
 		      Hello from a builder
 		    </body>
     </html>
-    <script>
-const queryString = window.location.search;
- console.log(queryString);
-</script>
-
-<script>
- console.log("Is this log on?");
-</script>
     `,
   };
 }
