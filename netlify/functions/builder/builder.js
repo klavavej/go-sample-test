6147578dd2a1aab9f41bb08e889e409ffa0d5f76
Kristen Lavavej <35638702+klavavej@users.noreply.github.com>
2021-12-17 08:02:52 -0800

    Update builder.js

--- netlify/functions/builder/builder.js
+++ netlify/functions/builder/builder.js
@@ -3,28 +3,11 @@
 const { builderFunction } = require("@netlify/functions");
 
 async function handler(event, context) {
-  const {test} = event.queryStringParameters;
+  const {test} = event.queryStringParameters
   console.log(test)
-  const time = new Date();
-
   return {
     statusCode: 200,
-     body: `
-    <!DOCTYPE html>
-	    <html>
-		    <body>
-		      Hello World
-		    </body>
-    </html>
-    <script>
-    const queryString = window.location.search;
-     console.log(queryString);
-    </script>
-    
-    <script>
-     console.log("Is this log on?");
-    </script>
-    `,
+    body: `Hello, ${test}`,
   };
 }
 
