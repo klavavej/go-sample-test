548972e8fe9426c13f2abb1461f0641f4e9b05b6
Kristen Lavavej <35638702+klavavej@users.noreply.github.com>
2021-12-17 08:25:05 -0800

    Update builder.js

--- netlify/functions/builder/builder.js
+++ netlify/functions/builder/builder.js
@@ -1,10 +1,8 @@
-
-   
 const { builderFunction } = require("@netlify/functions");
 
 async function handler(event, context) {
-  const {test} = event.queryStringParameters
-  console.log(test)
+  const { test } = event.queryStringParameters;
+  console.log(test);
   return {
     statusCode: 200,
     body: `Hello, ${test}`,
