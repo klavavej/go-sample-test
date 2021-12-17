dc8d9e2f9f72d71120d82894b6a2fc48e77a3a3f
Kristen Lavavej <35638702+klavavej@users.noreply.github.com>
2021-12-17 08:23:08 -0800

    Update echo.js

--- netlify/functions/echo/echo.js
+++ netlify/functions/echo/echo.js
@@ -1,6 +1,6 @@
 exports.handler = async function(event, context) {
-  const {test} = event.queryStringParameters
-  console.log(test)
+  const {test} = event.queryStringParameters;
+  console.log(test);
   return {
     statusCode: 200,
     body: `Hello, ${test}`,
