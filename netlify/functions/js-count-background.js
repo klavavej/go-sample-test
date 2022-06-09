7fc772758316a906c1d00d38ee77619a33565961
Kristen Lavavej <35638702+klavavej@users.noreply.github.com>
2022-06-09 11:45:27 -0700

    Create js-count-background.js

---
+++ netlify/functions/js-count-background.js
@@ -0,0 +1,17 @@
+exports.handler = async function (event, context) {
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    async function demo() {
+        for (let i = 0; i < 60; i++) {
+            let date = new Date();
+            await sleep(1000);
+            console.log(date.toLocaleString(), i);
+        }
+        console.log('Done');
+    };
+    
+    demo();
+}
