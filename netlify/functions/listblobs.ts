a4a75b77a6dc3794abe2dcfe3a6f4b8038641b86
Kristen Lavavej <35638702+klavavej@users.noreply.github.com>
2024-03-04 14:03:16 -0800

    Update listblobs.ts

--- netlify/functions/listblobs.ts
+++ netlify/functions/listblobs.ts
@@ -2,7 +2,7 @@ import { getStore } from "@netlify/blobs";
 import type { Context } from "@netlify/functions";
 
 export default async (req: Request, context: Context) => {
-  const beauty = getStore("animals");
+  const animals = getStore("animals");
   const { blobs } = await animals.list();
   
   console.log(blobs);
